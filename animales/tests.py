aeb86d746c33aefce9079826dbae01b467b9a45d
Yerco Jorquera <yjorquera@Yercos-Mac.local>
2021-01-16 14:15:49 +0100

    test crear fila de animales

--- animales/tests.py
+++ animales/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase, Client
+from urllib.parse import urlencode
 
 from .forms import AnimalForm
 from .models import Animales
@@ -8,7 +9,7 @@ from django.contrib.auth.models import User
 
 class AnimalesTestCase(TestCase):
 
-    def setUp(self) -> None:
+    def setUp(self):
         animales = Animales.objects.create(
             especie="Malamute",
             nombre="Los Jacos",
@@ -18,13 +19,22 @@ class AnimalesTestCase(TestCase):
         )
         animales.save()
         self.client = Client()
-
-    def test_validar_form(self):
         user = User.objects.create(username='testuser')
         user.set_password('12345')
         user.save()
+
+    def test_validar_form(self) -> None:
         logged_in = self.client.login(username='testuser', password='12345')
         self.assertEqual(True, logged_in)
         data = {'especie': 'Focas', 'nombre': 'Los Manolos', 'numero': 3, 'comida': 2, 'user_id': 1}
         form = AnimalForm(data=data)
         self.assertTrue(form.is_valid())
+
+    def test_crear_fila_de_animales(self):
+        logged_in = self.client.login(username='testuser', password='12345')
+        self.assertEqual(True, logged_in)
+        data = {'especie': 'Foca', 'nombre': 'Las Focas', 'numero': 3, 'comida': 2, 'user_id': 1}
+        self.client.post("/animales/", urlencode(data),
+                         content_type="application/x-www-form-urlencoded")
+        foca = Animales.objects.get(especie='Foca')
+        self.assertEqual('Foca', foca.especie)
