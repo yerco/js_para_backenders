242b08688f513290c2d918b321b0e858c52fb958
Calamandes <a@bc.de>
2021-02-13 15:32:08 +0100

    Haciendo objeto Helper privado y objeto AnimalesApp disponible

--- animales/static/animales/AnimalesApp.js
+++ animales/static/animales/AnimalesApp.js
@@ -1,72 +1,74 @@
-let AnimalesApp = {
-    initialize: function($wrapper) {
-        this.$wrapper = $wrapper;
+(function() {
+    window.AnimalesApp = {
+        initialize: function ($wrapper) {
+            this.$wrapper = $wrapper;
 
-        Helper.initialize($wrapper);
+            Helper.initialize($wrapper);
 
-        this.$wrapper.find('tbody tr').on(
-            'click',
-            this.handleRowClick.bind(this)
-        )
+            this.$wrapper.find('tbody tr').on(
+                'click',
+                this.handleRowClick.bind(this)
+            )
 
-        this.$wrapper.find('.js-delete-row').on(
-            'click',
-            this.handleComidaDelete.bind(this)
-        )
-    },
-    handleRowClick: function () {
-        console.log("Click en Fila");
-    },
-    handleComidaDelete: function(e) {
-        e.preventDefault();
+            this.$wrapper.find('.js-delete-row').on(
+                'click',
+                this.handleComidaDelete.bind(this)
+            )
+        },
+        handleRowClick: function () {
+            console.log("Click en Fila");
+        },
+        handleComidaDelete: function (e) {
+            e.preventDefault();
 
-        let $link = $(e.currentTarget);
+            let $link = $(e.currentTarget);
 
-        $link.addClass('text-danger');
-        $link.find('.fa')
-            .removeClass('fa-trash')
-            .addClass('fa-spinner')
-            .addClass('fa-spin');
+            $link.addClass('text-danger');
+            $link.find('.fa')
+                .removeClass('fa-trash')
+                .addClass('fa-spinner')
+                .addClass('fa-spin');
 
-        let deleteUrl = $link.data('url');
-        let $row = $link.closest('tr');
-        let that = this;
+            let deleteUrl = $link.data('url');
+            let $row = $link.closest('tr');
+            let that = this;
 
-        $.ajax({
-            url: deleteUrl,
-            method: 'POST',
-            data: { csrfmiddlewaretoken: csrftoken }
-        })
-            .done(function(data) {
-                console.log(data);
-                $row.fadeOut('normal', function () {
-                    $(this).remove();
-                    that.updateTotalComida();
+            $.ajax({
+                url: deleteUrl,
+                method: 'POST',
+                data: {csrfmiddlewaretoken: csrftoken}
+            })
+                .done(function (data) {
+                    console.log(data);
+                    $row.fadeOut('normal', function () {
+                        $(this).remove();
+                        that.updateTotalComida();
+                    });
+                })
+                .fail(function () {
+                    console.log("error");
+                })
+                .always(function () {
+                    console.log("completado");
                 });
-            })
-            .fail(function() {
-                console.log("error");
-            })
-            .always(function() {
-                console.log("completado");
-            });
-    },
-    updateTotalComida: function () {
-        this.$wrapper.find('.js-total-comida').html(Helper.calculateTotalComida());
-    }
-};
+        },
+        updateTotalComida: function () {
+            this.$wrapper.find('.js-total-comida').html(Helper.calculateTotalComida());
+        }
+    };
 
-let Helper = {
-    initialize: function($wrapper) {
-        this.$wrapper = $wrapper;
-    },
-    calculateTotalComida: function(){
-        let total = 0;
-        this.$wrapper.find('tbody tr').each(function () {
-            if (undefined !== $(this).data('comida')) {
-                total += parseInt($(this).data('comida'));
-            }
-        });
-        return total;
+    let Helper = {
+        initialize: function ($wrapper) {
+            this.$wrapper = $wrapper;
+        },
+        calculateTotalComida: function () {
+            let total = 0;
+            this.$wrapper.find('tbody tr').each(function () {
+                if (undefined !== $(this).data('comida')) {
+                    total += parseInt($(this).data('comida'));
+                }
+            });
+            return total;
+        }
     }
-}
+})();
